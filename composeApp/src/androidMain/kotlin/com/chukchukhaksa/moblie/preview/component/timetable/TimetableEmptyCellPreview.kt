b44354b6785dcb4ac0201d174cb7e108279dcbd6
jinukeu <koownij@kakao.com>
2025-05-21 23:41:48 +0900

    feat: Timetable 컴포넌트 이사

---
+++ composeApp/src/androidMain/kotlin/com/chukchukhaksa/moblie/preview/component/timetable/TimetableEmptyCellPreview.kt
@@ -0,0 +1,23 @@
+package com.chukchukhaksa.moblie.preview.component.timetable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chukchukhaksa.moblie.common.designsystem.theme.SuwikiTheme
+import com.chukchukhaksa.moblie.presentation.timetable.component.timetable.cell.EmptyCell
+
+@Preview(showBackground = true)
+@Composable
+fun TimetableEmptyCellPreview() {
+    SuwikiTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            EmptyCell()
+
+            EmptyCell(text = "월")
+        }
+    }
+}
